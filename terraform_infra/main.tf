4fac03f9bdd8289a66924e8145cb956dd14360ce
Clinton <clinton.pillay@outlook.com>
2026-06-23 23:13:31 +0200

    added go package, and automated docker build

--- terraform_infra/main.tf
+++ terraform_infra/main.tf
@@ -38,9 +38,9 @@ resource "aws_instance" "minikube_ec2" {
               MY_IP=$(curl -s https://checkip.amazonaws.com)
               curl -s "https://www.duckdns.org/update?domains=${var.duckdns_domain}&token=${var.duckdns_token}&ip=$MY_IP"
               
-              sudo mkdir -p /usr/local/lib/docker/cli-plugins
-              sudo curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
-              sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+              mkdir -p /usr/local/lib/docker/cli-plugins
+              curl -L https://github.com/docker/buildx/releases/download/v0.17.1/buildx-v0.17.1.linux-amd64 -o /usr/local/lib/docker/cli-plugins/docker-buildx
+              chmod +x /usr/local/lib/docker/cli-plugins/docker-buildx
 
               mkdir -p /home/ec2-user/.docker/cli-plugins
               curl -L https://github.com/docker/buildx/releases/download/v0.17.1/buildx-v0.17.1.linux-amd64 -o /home/ec2-user/.docker/cli-plugins/docker-buildx
@@ -55,6 +55,13 @@ resource "aws_instance" "minikube_ec2" {
               echo 'SERVICE_NODE_OPTIONS='--max-old-space-size=3072'' >> /home/ec2-user/central/.env
               echo DOMAIN=${var.duckdns_domain} >> /home/ec2-user/central/.env
               echo SYSADMIN_EMAIL=${var.sysadmin_email} >> /home/ec2-user/central/.env
+              echo CENTRAL_WEBHOOK_NEW_SUBMISSION_URL=http://host.docker.internal:5000/webhook  >> /home/ec2-user/central/.env
+              echo CENTRAL_WEBHOOK_REVIEW_SUBMISSION_URL=http://host.docker.internal:5000/webhook >> /home/ec2-user/central/.env
+              echo CENTRAL_WEBHOOK_UPDATE_ENTITY_URL=http://host.docker.internal:5000/webhook >> /home/ec2-user/central/.env
+              echo CENTRAL_WEBHOOK_API_KEY=your-secret-api-key >> /home/ec2-user/central/.env
+              echo CENTRAL_WEBHOOK_LOG_LEVEL=INFO >> /home/ec2-user/central/.env
+              echo CENTRAL_WEBHOOK_TAG=latest >> /home/ec2-user/central/.env
+              echo POSTGRES_MAJOR=14 >> /home/ec2-user/central/.env
               htpasswd -bc /home/ec2-user/central/files/nginx/prometheus.htpasswd admin ${var.prometheus_pass}
               echo GRAFANA_USER=admin >> /home/ec2-user/central/.env
               echo GRAFANA_PASSWORD=${var.grafana_pass} >> /home/ec2-user/central/.env
@@ -71,6 +78,35 @@ resource "aws_instance" "minikube_ec2" {
               chown -R ec2-user:ec2-user /home/ec2-user/.docker
               chown -R ec2-user:ec2-user /home/ec2-user/central
               chmod -R u=rwX,go=rX /home/ec2-user/central
+
+              curl -OL https://go.dev/dl/go1.25.5.linux-amd64.tar.gz
+              sudo tar -C /usr/local -xzf go1.25.5.linux-amd64.tar.gz
+              export PATH=/usr/local/go/bin:$PATH
+              export GOPATH=/root/go
+              export GOMODCACHE=/root/go/pkg/mod
+
+              cd /home/ec2-user/central/central-webhook
+              go build -o centralwebhook .
+
+
+              cd /home/ec2-user/central
+              /usr/local/lib/docker/cli-plugins/docker-compose -f docker-compose.yml -f /home/ec2-user/central/central-webhook/compose.webhook.yml up -d
+
+
+              echo "Waiting for PostgreSQL to be ready..."
+              until docker exec central-postgres14-1 pg_isready -U odk; do
+                echo "PostgreSQL not ready, waiting..."
+                sleep 5
+              done
+
+              ./centralwebhook install \
+                  -db 'postgresql://odk:odk@localhost:5432/odk?sslmode=disable' \
+                  -newSubmissionUrl 'http://172.17.0.1:5000/webhook' \
+                  -reviewSubmissionUrl 'http://172.17.0.1:5000/webhook' \
+                  -updateEntityUrl 'http://172.17.0.1:5000/webhook'
+
+              echo "ODK Central setup complete!"
+
               EOF
 
   tags = {
